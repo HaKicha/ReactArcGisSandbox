1ae988e6560d36d473c525f1878b552ea63c817a
HaKicha <ivankika.1999@gmail.com>
2019-02-22 12:57:52 +0200

    Added test-case file temo.js

---
+++ src/resources/temp.js
@@ -0,0 +1,18 @@
+export default function getData() {
+    return `[
+{"id":1,"actionType":"Event1","source":"twitter","victims":28,"injured":5,"geoData":"{\\"type\\":\\"Point\\",\\"coordinates\\":[43.4874,34.9302]}","link":"http://www.google.com","timestamp":"2019-02-16T11:55:13.909Z"},
+{"id":2,"actionType":"Event1","source":"facebook","victims":15,"injured":2,"geoData":"{\\"type\\":\\"Point\\",\\"coordinates\\":[43.2637,33.4324]}","link":"","timestamp":"2019-02-16T11:55:13.936Z"},
+{"id":3,"actionType":"Event2","source":"nato","victims":14,"injured":7,"geoData":"{\\"type\\":\\"Point\\",\\"coordinates\\":[39.094,35.3246]}","link":"","timestamp":"2019-02-16T11:55:13.936Z"},
+{"id":4,"actionType":"Event3","source":"news","victims":7,"injured":3,"geoData":"{\\"type\\":\\"Point\\",\\"coordinates\\":[43.1171,36.3246]}","link":"","timestamp":"2019-02-16T11:55:13.936Z"},
+{"id":5,"actionType":"Event4","source":"gichd","victims":18,"injured":2,"geoData":"{\\"type\\":\\"Point\\",\\"coordinates\\":[41.851,35.009]}","link":"","timestamp":"2019-02-16T11:55:13.936Z"},
+{"id":6,"actionType":"Event4","source":"gichd","victims":14,"injured":3,"geoData":"{\\"type\\":\\"Point\\",\\"coordinates\\":[40.7653,35.4959]}","link":"","timestamp":"2019-02-16T11:55:13.937Z"},
+{"id":7,"actionType":"Event4","source":"news","victims":1,"injured":0,"geoData":"{\\"type\\":\\"Point\\",\\"coordinates\\":[39.4874,33.9302]}","link":"","timestamp":"2019-02-16T11:55:13.937Z"},
+{"id":8,"actionType":"Event","source":"facebook","victims":26,"injured":3,"geoData":"{\\"type\\":\\"Point\\",\\"coordinates\\":[40.7821,33.3516]}","link":"","timestamp":"2019-02-16T11:55:13.937Z"},
+{"id":9,"actionType":"Event","source":"twitter","victims":9,"injured":3,"geoData":"{\\"type\\":\\"Point\\",\\"coordinates\\":[41.1171,36.3246]}","link":"","timestamp":"2019-02-16T11:55:13.937Z"},
+{"id":10,"actionType":"Event","source":"nato","victims":17,"injured":5,"geoData":"{\\"type\\":\\"Point\\",\\"coordinates\\":[38.3535,36.8909]}","link":"","timestamp":"2019-02-16T11:55:13.937Z"},
+{"id":12,"actionType":"Event","source":"nato","victims":17,"injured":5,"geoData":"{\\"type\\":\\"LineString\\",\\"coordinates\\":[[38.3535,36.8909],[39.094,35.3246],[43.1171,36.3246]]}","link":"","timestamp":"2019-02-16T11:55:13.937Z"},
+{"id":11,"actionType":"Event","source":"nato","victims":17,"injured":5,"geoData":"{\\"type\\":\\"Polygon\\",\\"coordinates\\":[[43.4874,34.9302],[43.2637,33.4324],[39.094,35.3246],[43.4874,34.9302]]}","link":"","timestamp":"2019-02-16T11:55:13.937Z"}
+
+]
+`
+};
